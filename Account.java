f01ffda71d056611191130cc65180d77e48ef2b6
manjari11 <manjari.rastogi11@gmail.com>
2024-04-30 18:57:27 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,44 @@
+package com.assignment.atmcashwithdrawl.pojos;
+
+public class Account {
+
+	private String name;
+	private int accNumber;
+	private double balance;
+	
+	public Account(String name,int accNumber,double balance) {
+		this.name = name;
+		this.accNumber = accNumber;
+		this.balance = balance;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAccNumber() {
+		return accNumber;
+	}
+
+	public void setAccNumber(int accNumber) {
+		this.accNumber = accNumber;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+
+	public String toString() {
+		return "Owner Name :: " + name + " || Account Number :: " + accNumber + " || Balance Amount :: " + balance;
+	}
+
+}
